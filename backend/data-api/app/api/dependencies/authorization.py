f39f1b7067a4d782c4dc1ff5ce1637e01a6910b8
Marcus Scaffidi <mscaffidi6543@gmail.com>
2024-07-04 08:35:38 -0500

    Auth change and iteration tests

--- backend/data-api/app/api/dependencies/authorization.py
+++ backend/data-api/app/api/dependencies/authorization.py
@@ -4,15 +4,17 @@ from typing import Any, Dict
 from fastapi import HTTPException, Request
 from fastapi.security import SecurityScopes
 from jwt import decode, encode
-from jwt.exceptions import ExpiredSignatureError
-from passlib.exc import InvalidTokenError
-from pydantic import ValidationError
 
 from app.api.models.users import TokenData
 from app.api.settings import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
 from app.database.users.models.user import User
 from app.database.users.models.user_permission import UserRoleType
 from app.exceptions.common import ObjectNotFoundException
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 PERMISSION_LEVELS = {
     UserRoleType.ADMIN: 3,
@@ -55,7 +57,9 @@ async def get_current_user(request: Request, security_scopes: SecurityScopes):
 
         token_scopes = payload.get("scopes", [])
         token_data = TokenData(scopes=token_scopes, username=username)
-    except (InvalidTokenError, ValidationError, ExpiredSignatureError):
+    except Exception as e:
+        logger.exception("Unable to verify access token")
+
         raise credentials_exception
 
     try:
--- backend/data-api/tests/acceptance/api/routers/test_iterations.py
+++ backend/data-api/tests/acceptance/api/routers/test_iterations.py
@@ -39,3 +39,51 @@ async def test_should_not_get_iteration_with_expired_token(data_api: TestClient)
 
     response = await data_api.get(f"{organization['id']}/iterations/{iteration['id']}", headers=expired_headers)
     assert response.status_code == 401
+
+
+async def test_should_update_iteration(data_api: TestClient):
+    _, _, headers = await authenticate(data_api, create_org=False)
+    organization = await add_organization(data_api, headers=headers)
+    iteration = await add_iteration(data_api, organization_id=organization["id"], headers=headers)
+
+    data = {"title": "Test Updated"}
+    response = await data_api.patch(f"{organization['id']}/iterations/{iteration['id']}", json=data, headers=headers)
+
+    assert response.status_code == 200
+    iteration = response.json()
+    assert iteration["title"] == "Test Updated"
+
+
+async def test_should_not_update_iteration_with_expired_headers(data_api: TestClient):
+    _, _, headers = await authenticate(data_api, create_org=False)
+    organization = await add_organization(data_api, headers=headers)
+    iteration = await add_iteration(data_api, organization_id=organization["id"], headers=headers)
+
+    data = {"title": "Test Updated"}
+    response = await data_api.patch(f"{organization['id']}/iterations/{iteration['id']}", json=data, headers=expired_headers)
+
+    assert response.status_code == 401
+
+
+async def test_should_delete_iteration(data_api: TestClient):
+    _, _, headers = await authenticate(data_api, create_org=False)
+    organization = await add_organization(data_api, headers=headers)
+    iteration = await add_iteration(data_api, organization_id=organization["id"], headers=headers)
+
+    response = await data_api.delete(f"{organization['id']}/iterations/{iteration['id']}", headers=headers)
+    assert response.status_code == 200
+
+    response = await data_api.get(f"{organization['id']}/iterations/{iteration['id']}", headers=headers)
+    assert response.status_code == 404
+
+
+async def test_should_not_delete_iteration_with_expired_headers(data_api: TestClient):
+    _, _, headers = await authenticate(data_api, create_org=False)
+    organization = await add_organization(data_api, headers=headers)
+    iteration = await add_iteration(data_api, organization_id=organization["id"], headers=headers)
+
+    response = await data_api.delete(f"{organization['id']}/iterations/{iteration['id']}", headers=expired_headers)
+    assert response.status_code == 401
+
+    response = await data_api.get(f"{organization['id']}/iterations/{iteration['id']}", headers=headers)
+    assert response.status_code == 200
