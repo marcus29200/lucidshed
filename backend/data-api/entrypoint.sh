efdc39e6410815e20fe46395f952bc37c6ee4349
Marcus Scaffidi <mscaffidi6543@gmail.com>
2024-08-18 07:35:49 -0500

    Debugging

---
+++ backend/data-api/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ensure poetry is available
+echo "Current PATH: $PATH"
+# Ensure poetry is available
+export PATH="$PATH:/root/.local/bin"
+# Log the modified PATH
+echo "Modified PATH: $PATH"
+
+poetry run api
