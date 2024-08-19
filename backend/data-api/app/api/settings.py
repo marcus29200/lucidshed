77ef5f1e01abc9197a789280047873d1235b8835
Marcus Scaffidi <mscaffidi6543@gmail.com>
2024-08-18 22:06:56 -0500

    Cleanup/logging

--- backend/data-api/app/api/settings.py
+++ backend/data-api/app/api/settings.py
@@ -35,11 +35,13 @@ class Settings(BaseModel):
         db_name = db_name or self.user_db_name
 
         if self.database_connection_name:
-            url = f"postgresql://{self.database_user}:{self.database_password}@localhost/{db_name}?host=/cloudsql/{self.database_connection_name}"
+            url = f"postgresql://{self.database_user}:{self.database_password}@{self.database_host}:{self.database_port}/{db_name}?host=/cloudsql/{self.database_connection_name}"
         else:
             url = f"postgresql://{self.database_user}:{self.database_password}@{self.database_host}:{self.database_port}/{db_name}"
 
         logger.error(f"Database URL: {url}")
+
+        return url
 
 
 settings = Settings()
