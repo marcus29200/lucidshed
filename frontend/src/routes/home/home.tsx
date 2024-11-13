28c7b44463bead8136c5f2ae6e1a896ec45f1e04
Ricardo Jimenez E <jimenezesricardo@gmail.com>
2024-11-12 21:14:49 -0700

    better positioning

--- frontend/src/routes/home/home.tsx
+++ frontend/src/routes/home/home.tsx
@@ -1,16 +1,44 @@
-import { Button, Container } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Home() {
 	return (
-		<div
-			style={{
-				backgroundImage: `url(${import.meta.env.BASE_URL + '/background.png'})`,
-				backgroundSize: 'cover',
-				backgroundPosition: 'center',
-				height: '100vh',
-			}}
-		>
+		<div className="grid md:grid-cols-2 h-screen">
+			<Box
+				sx={{
+					position: 'relative',
+					width: '100%',
+					height: '100vh',
+					':before': {
+						content: '""',
+						backgroundImage: `url(${
+							import.meta.env.BASE_URL + '/background.png'
+						})`,
+						backgroundSize: 'contain',
+						backgroundPosition: 'center',
+						backgroundRepeat: 'no-repeat',
+						width: '100%',
+						height: '100%',
+						display: 'block',
+						position: 'absolute',
+					},
+					':after': {
+						content: '""',
+						backgroundImage: `url(${
+							import.meta.env.BASE_URL + '/background.png'
+						})`,
+						backgroundSize: 'contain',
+						backgroundPosition: 'center',
+						backgroundRepeat: 'no-repeat',
+						width: '100%',
+						height: '100%',
+						display: 'block',
+						filter: 'blur(200px)',
+						position: 'absolute',
+						zIndex: '-1',
+					},
+				}}
+			></Box>
 			<Container
 				style={{
 					display: 'flex',
@@ -18,6 +46,8 @@ function Home() {
 					alignItems: 'center',
 					padding: '4rem',
 					gap: '2rem',
+					justifyContent: 'center',
+					height: '100vh',
 				}}
 			>
 				<img src={import.meta.env.BASE_URL + '/logo.svg'} />
