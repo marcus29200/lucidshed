4a1dd51f21ae573a32af4bc0deda54cc52e95446
Ricardo Jimenez E <jimenezesricardo@gmail.com>
2024-11-12 20:12:24 -0700

    add background to home page

--- frontend/src/routes/home/home.tsx
+++ frontend/src/routes/home/home.tsx
@@ -3,27 +3,50 @@ import { Link } from 'react-router-dom';
 
 function Home() {
 	return (
-		<Container
+		<div
 			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				padding: '4rem',
-				gap: '2rem',
+				backgroundImage: `url(${import.meta.env.BASE_URL + '/background.png'})`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				height: '100vh',
 			}}
 		>
-			<img src={import.meta.env.BASE_URL + '/logo.svg'} />
-			<Link to="/login">
-				<Button color="primary" variant="contained">
-					Log in
-				</Button>
-			</Link>
-			<Link to="/register">
-				<Button color="primary" variant="contained">
-					Sign up
-				</Button>
-			</Link>
-		</Container>
+			<Container
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					padding: '4rem',
+					gap: '2rem',
+				}}
+			>
+				<img src={import.meta.env.BASE_URL + '/logo.svg'} />
+				<Link to="/login">
+					<Button
+						color="primary"
+						variant="contained"
+						sx={{
+							width: '150px',
+							paddingY: '8px !important',
+						}}
+					>
+						Log in
+					</Button>
+				</Link>
+				<Link to="/register">
+					<Button
+						color="primary"
+						variant="contained"
+						sx={{
+							width: '150px',
+							paddingY: '8px !important',
+						}}
+					>
+						Sign up
+					</Button>
+				</Link>
+			</Container>
+		</div>
 	);
 }
 
