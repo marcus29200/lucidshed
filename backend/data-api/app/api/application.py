8356007ce526987004d9f8a073843901edda5761
Marcus Scaffidi <mscaffidi6543@gmail.com>
2024-11-15 09:59:25 -0600

    Logging for opensearch

--- backend/data-api/app/api/application.py
+++ backend/data-api/app/api/application.py
@@ -91,6 +91,7 @@ class DataApplication(FastAPI):
         self.history_controller = HistoryController()
         self.file_controller = FileController()
 
+        logger.info(f"Initializing opensearch client with {settings.opensearch_host}:{settings.opensearch_port}")
         self.opensearch_client = OpenSearch(
             hosts=[f"{settings.opensearch_host}:{settings.opensearch_port}"],
             http_auth=(settings.opensearch_username, settings.opensearch_password),
