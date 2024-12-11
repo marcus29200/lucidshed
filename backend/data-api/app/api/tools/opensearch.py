4918477bd966991709227662170e3effec4437d2
Marcus Scaffidi <mscaffidi6543@gmail.com>
2024-12-10 19:40:08 -0600

    More logging/error handling

--- backend/data-api/app/api/tools/opensearch.py
+++ backend/data-api/app/api/tools/opensearch.py
@@ -15,20 +15,19 @@ async def index_object(
     document: Optional[Dict[str, Any]] = None,
     mode: Optional[str] = "create",
 ) -> bool:
-    if not settings.opensearch_enabled:
-        logger.warning(f"OpenSearch is not enabled, {item_id} not indexed")
+    if settings.opensearch_enabled:
+        logger.info(f"Indexing object {item_id} in index {index}")
+        if settings.opensearch_async_indexing:
+            asyncio.create_task(_index_object(opensearch_client, index, item_id, document, mode))
 
-        return False
-
-    if settings.opensearch_async_indexing:
-        asyncio.create_task(_index_object(opensearch_client, index, item_id, document, mode))
-
-        return True
-    else:
-        result = await _index_object(opensearch_client, index, item_id, document, mode)
-
-        if result["result"] in ["created", "updated", "deleted"]:
             return True
+        else:
+            result = await _index_object(opensearch_client, index, item_id, document, mode)
+
+            if result["result"] in ["created", "updated", "deleted"]:
+                return True
+    else:
+        logger.warning(f"OpenSearch is not enabled, unable to index object {item_id}")
 
     return False
 
@@ -40,12 +39,15 @@ async def _index_object(
     document: Optional[Dict[str, Any]] = None,
     mode: Optional[str] = "create",
 ) -> Dict[str, Any]:
-    match mode:
-        case "create":
-            return opensearch_client.index(index=index, id=item_id, body=document)
-        case "update":
-            return opensearch_client.update(index=index, id=item_id, body=document)
-        case "delete":
-            return opensearch_client.delete(index=index, id=item_id)
-        case _:
-            raise ValueError(f"Unable to index using {mode}")
+    try:
+        match mode:
+            case "create":
+                return opensearch_client.index(index=index, id=item_id, body=document)
+            case "update":
+                return opensearch_client.update(index=index, id=item_id, body=document)
+            case "delete":
+                return opensearch_client.delete(index=index, id=item_id)
+            case _:
+                raise ValueError(f"Unable to index using {mode}")
+    except Exception:
+        logger.exception("Unable to index object")
